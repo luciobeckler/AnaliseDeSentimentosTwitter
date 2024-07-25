b015012e7525862f74d11f5b19c26f1db561bd60
Fabian Bernardes <fabiancb2001@gmail.com>
2024-07-25 08:28:43 -0300

    adição da chamada dos arquivos referente a outros idiomas restantes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 
 import ImplementacaoLista.ListaSe;
 
@@ -9,6 +8,18 @@ public class Main {
 
         ListaSe listaAlbano = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Albanian_Twitter_sentiment.csv");
         ListaSe listaAlemao = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/German_Twitter_sentiment.csv");
-
+        ListaSe listaRUssa = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Russian_Twitter_sentiment.csv");
+        ListaSe listaServia = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Serbian_Twitter_sentiment.csv");
+        ListaSe listaEspanha = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Spanish_Twitter_sentiment.csv");
+        ListaSe listaBosnia = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Bosnian_Twitter_sentiment.csv");
+        ListaSe listaBulgaria = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Bulgarian_Twitter_sentiment.csv");
+        ListaSe listaCroacia = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Croatian_Twitter_sentiment.csv");
+        ListaSe listaIngles = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/English_Twitter_sentiment.csv");
+        ListaSe listahungara = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Hungarian_Twitter_sentiment.csv");
+        ListaSe listaPortugues = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Portugueses_Twitter_sentiment.csv");
+        ListaSe listaPolones = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Polish_Twitter_sentiment.csv");
+        ListaSe listaEslovaco = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Slovak_Twitter_sentiment.csv");
+        ListaSe listaEslovenia = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Slovenian_Twitter_sentiment.csv");
+        ListaSe listaSueca = leitorDeArquivos.LeArquivosECriaObjetos("src/Data/Swedish_Twitter_sentiment.csv");
     }
 }
